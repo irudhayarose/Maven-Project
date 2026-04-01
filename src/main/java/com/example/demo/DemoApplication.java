3ae986d8ed0e40ea093d53de275d3bd260be106f
irudhaya rose A <160041264+irudhayarose@users.noreply.github.com>
2026-04-01 09:31:02 +0530

    Add name variable to DemoApplication class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     int age;
+	int name;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
